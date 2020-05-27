957379801a6b0dc257d51397bb145db6b1096bfb
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-05-27 15:56:28 +0530

    Created Cat class

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:catinthebox/src/widgets/cat.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -17,6 +18,6 @@ class _HomeState extends State<Home> {
   }
 
   Widget buildAnimation() {
-
+    return Cat();
   }
 }
